a09570894b1c4caf5e8f9c0ae5ade4fa8cb900ce
Jenny Williams <weisenje@research.ge.com>
2017-01-20 10:29:19 -0500

    Removed TestGraph from unit test

--- sparqlGraphLibrary/src/test/java/com/ge/research/semtk/belmont/test/DataToModelTransformerTest.java
+++ sparqlGraphLibrary/src/test/java/com/ge/research/semtk/belmont/test/DataToModelTransformerTest.java
@@ -6,12 +6,11 @@
 package com.ge.research.semtk.belmont.test;
 
 import static org.junit.Assert.assertEquals;
-import org.junit.BeforeClass;
+
 import org.junit.Test;
 
 import java.util.ArrayList;
 
-import com.ge.research.semtk.test.TestGraph;
 import com.ge.research.semtk.belmont.Node;
 import com.ge.research.semtk.belmont.NodeGroup;
 import com.ge.research.semtk.belmont.PropertyItem;
@@ -19,15 +18,9 @@ import com.ge.research.semtk.load.dataset.CSVDataset;
 import com.ge.research.semtk.load.dataset.Dataset;
 import com.ge.research.semtk.load.utility.DataToModelTransformer;
 import com.ge.research.semtk.load.utility.SparqlGraphJson;
+import com.ge.research.semtk.load.utility.Utility;
 
 public class DataToModelTransformerTest {
-	
-	private static SparqlGraphJson sgJson = null;
-	
-	@BeforeClass
-	public static void beforeClass() throws Exception {
-		sgJson = TestGraph.initGraphWithData("sampleBattery");
-	}
 
 	@Test
 	public void checkSpecHandlerFunction() throws Exception {
@@ -35,30 +28,31 @@ public class DataToModelTransformerTest {
 		String[] headers = { "Battery", "Cell", "color", "birthday" };
 		Dataset ds = new CSVDataset("src/test/resources/sampleBattery.csv",	headers);
 
+		SparqlGraphJson sgJson = new SparqlGraphJson(Utility.getJSONObjectFromFilePath("src/test/resources/sampleBattery.json"));
 		DataToModelTransformer dtmx = new DataToModelTransformer(sgJson, 6, ds);
 		ArrayList<NodeGroup> ngArr = dtmx.getNextBatch();
 	
-		for (NodeGroup n : ngArr) {
-			for (Node nd : n.getNodeList()) {
-				System.out.println(nd.getSparqlID() + " instance value was " + nd.getInstanceValue());
-				for (PropertyItem prop : nd.getPropertyItems()) {
-					String ret = "";
-					for (String inst : prop.getInstanceValues()) {
-						ret += inst + ",";
-					}
-					if (ret != "") {
-						System.out.println("\t" + prop.getUriRelationship()	+ " ----> " + ret);
-					}
-				}
-			}
-		}		
+//		for (NodeGroup n : ngArr) {
+//			for (Node nd : n.getNodeList()) {
+//				System.out.println(nd.getSparqlID() + " instance value was " + nd.getInstanceValue());
+//				for (PropertyItem prop : nd.getPropertyItems()) {
+//					String ret = "";
+//					for (String inst : prop.getInstanceValues()) {
+//						ret += inst + ",";
+//					}
+//					if (ret != "") {
+//						System.out.println("\t" + prop.getUriRelationship()	+ " ----> " + ret);
+//					}
+//				}
+//			}
+//		}		
 
 		assertEquals(ngArr.size(),4);		
 		assertEquals(ngArr.get(0).getNodeCount(),3);
 		assertEquals(ngArr.get(0).getNodeList().get(0).getFullUriName(),"http://kdl.ge.com/batterydemo#Color");
 		assertEquals(ngArr.get(0).getNodeList().get(1).getFullUriName(),"http://kdl.ge.com/batterydemo#Cell");
 		assertEquals(ngArr.get(0).getNodeList().get(2).getFullUriName(),"http://kdl.ge.com/batterydemo#Battery");
-		assertEquals(ngArr.get(0).getNodeList().get(0).getInstanceValue(),"http://kdl.ge.com/batterydemo#red");
+		assertEquals(ngArr.get(0).getNodeList().get(0).getInstanceValue(),"belmont/generateSparqlInsert#red");
 		assertEquals(ngArr.get(0).getNodeList().get(1).getInstanceValue(),"belmont/generateSparqlInsert#Cell_cell200");
 		assertEquals(ngArr.get(0).getNodeList().get(2).getInstanceValue(),"belmont/generateSparqlInsert#Battery_battA");
 		assertEquals(ngArr.get(0).getNodeList().get(1).getPropertyItems().size(),1);
