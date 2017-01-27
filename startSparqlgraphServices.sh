b0a1cbc3f11c7b6647a695fb6bf720caf268aaa3
Jenny Williams <weisenje@research.ge.com>
2017-01-27 09:44:42 -0500

    Corrected obvious mismatches in commands to start ontologyInfoService and nodeGroupStoreService

--- startSparqlgraphServices.sh
+++ startSparqlgraphServices.sh
@@ -5,7 +5,7 @@
 
 PORT_SPARQL_QUERY_SERVICE=12050
 PORT_SPARQL_ONTOLOGY_INFO_SERVICE=12057
-PORT_SPARQL_NODE_GROUP_SERVICE=12058
+PORT_SPARQL_NODE_GROUP_STORE_SERVICE=12058
 PORT_INGESTION_SERVICE=12091
 
 if [ -z ${JAVA_HOME} ]; then
@@ -21,12 +21,13 @@ echo $SEMTK
 mkdir -p $LOGS
 
 echo "=== START MICROSERVICES... ==="
+
 $JAVA_HOME/bin/java -jar $SEMTK/sparqlQueryService/target/sparqlQueryService-1.1.0-SNAPSHOT.jar --server.port=$PORT_SPARQL_QUERY_SERVICE > $LOGS/sparqlQueryService.log 2>&1 &
 
 $JAVA_HOME/bin/java -jar $SEMTK/sparqlGraphIngestionService/target/sparqlGraphIngestionService-1.1.0-SNAPSHOT.jar --spring.config.location=$SEMTK/sparqlGraphIngestionService/src/main/resources/ingest.properties --server.port=$PORT_INGESTION_SERVICE --multipart.maxFileSize=1000Mb > $LOGS/sparqlGraphIngestionService.log 2>&1 &
 
-$JAVA_HOME/bin/java -jar $SEMTK/ontologyInfoService/target/ontologyInfoService-1.1.0-SNAPSHOT.jar --spring.config.location=$SEMTK/sparqlGraphIngestionService/src/main/resources/ingest.properties --server.port=$PORT_ONTOLOGY_INFO_SERVICE --multipart.maxFileSize=1000Mb > $LOGS/sparqlGraphIngestionService.log 2>&1 &
+$JAVA_HOME/bin/java -jar $SEMTK/ontologyInfoService/target/ontologyInfoService-1.1.0-SNAPSHOT.jar --spring.config.location=$SEMTK/ontologyInfoService/src/main/resources/ontologyinfo.properties --server.port=$PORT_ONTOLOGY_INFO_SERVICE --multipart.maxFileSize=1000Mb > $LOGS/ontologyInfoService.log 2>&1 &
 
-$JAVA_HOME/bin/java -jar $SEMTK/nodeGroupStoreService/target/nodeGroupStoreService-1.1.0-SNAPSHOT.jar --spring.config.location=$SEMTK/sparqlGraphIngestionService/src/main/resources/ingest.properties --server.port=$PORT_NODE_GROUP_SERVICE --multipart.maxFileSize=1000Mb > $LOGS/sparqlGraphIngestionService.log 2>&1 &
+$JAVA_HOME/bin/java -jar $SEMTK/nodeGroupStoreService/target/nodeGroupStoreService-1.1.0-SNAPSHOT.jar --spring.config.location=$SEMTK/nodeGroupStoreService/src/main/resources/store.properties --server.port=$PORT_SPARQL_NODE_GROUP_STORE_SERVICE --multipart.maxFileSize=1000Mb > $LOGS/nodeGroupStoreService.log 2>&1 &
 
 echo "=== DONE ==="
