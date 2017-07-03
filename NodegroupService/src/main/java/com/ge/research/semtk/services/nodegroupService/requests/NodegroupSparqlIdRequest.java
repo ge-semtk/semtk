9ae489adf076da364cfcac25169bf8cd833c5324
Paul Cuddihy <cuddihy@research.ge.com>
2017-07-03 13:03:18 -0400

    added generateFilter endpoint to NodeGroup Service

---
+++ NodegroupService/src/main/java/com/ge/research/semtk/services/nodegroupService/requests/NodegroupSparqlIdRequest.java
@@ -0,0 +1,14 @@
+package com.ge.research.semtk.services.nodegroupService.requests;
+
+public class NodegroupSparqlIdRequest extends NodegroupRequest {
+
+	private String sparqlId;
+
+	public String getSparqlId() {
+		return sparqlId;
+	}
+
+	public void setSparqlId(String sparqlId) {
+		this.sparqlId = sparqlId;
+	}
+}
