a03635b06020200c24cc10d6b2a78f32c479631c
McHugh <200018594@logon.ds.ge.com>
2017-07-06 16:03:23 -0400

    adding the ping endpoint to everything

---
+++ ontologyInfoService/src/main/java/com/ge/research/semtk/services/ontologyinfo/ServiceAvailabilityInformation.java
@@ -0,0 +1,35 @@
+package com.ge.research.semtk.services.ontologyinfo;
+
+import org.json.simple.JSONObject;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ge.research.semtk.resultSet.SimpleResultSet;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/serviceInfo")
+public class ServiceAvailabilityInformation {
+
+	
+	@CrossOrigin
+	@RequestMapping(value="/ping", method= RequestMethod.POST)
+	public JSONObject ping(){
+		
+		SimpleResultSet retval = null;
+		
+		try{
+			retval = new SimpleResultSet(true);
+			retval.addResult("available", "yes");
+			
+		}
+		catch(Exception eee){
+			retval = new SimpleResultSet(false, eee.getMessage());
+			eee.printStackTrace();
+		}
+		
+		return retval.toJson();
+	}
+}
