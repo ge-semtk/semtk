ca72d5f94778247753b7d83c21ddff4994301ba0
McHugh <200018594@logon.ds.ge.com>
2017-07-07 13:29:56 -0400

    adding deletion of results on a clock as part of the results service

---
+++ sparqlGraphResultsService/src/main/java/com/ge/research/semtk/services/results/cleanUp/DeleteThread.java
@@ -0,0 +1,68 @@
+package com.ge.research.semtk.services.results.cleanUp;
+
+import java.io.File;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ge.research.semtk.edc.JobTracker;
+import com.ge.research.semtk.services.results.ResultsEdcConfigProperties;
+
+public class DeleteThread extends Thread {
+
+	@Autowired
+	ResultsEdcConfigProperties edc_prop;
+	
+	private int runFrequencyInMilliseconds;
+	private File locationToDeleteFrom;	
+	private JobTracker jTracker;
+	
+	public DeleteThread(String fileStorageLocation, int frequencyInMinutes){
+		this.locationToDeleteFrom = new File(fileStorageLocation);
+		this.runFrequencyInMilliseconds = frequencyInMinutes * 60 * 1000;
+		try {
+			this.jTracker = new JobTracker(edc_prop);
+		} catch (Exception e) {
+			// something failed when getting the jobtracker. report it but continue anyway
+			System.err.println("unable to get a job tracker instance. reason given: " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+			
+    public void run() {
+    	
+    	System.err.println("Clean up initialized...");
+    	
+    	while(true){
+    		try{
+    			
+    			// cleanup files.
+    	    	
+    			long now = new Date().getTime();
+    			long cutoff = now - this.runFrequencyInMilliseconds;
+    			System.err.println("Clean up started...");
+    			
+    			for (File f : locationToDeleteFrom.listFiles()) {
+	                 if (f.lastModified() < cutoff) {
+	                	 f.delete();
+	                 }
+                }
+    			
+    			// cleanup meta data.
+    			
+    			
+    		}
+    		catch(Exception iei){
+    			System.err.println(iei.getMessage());
+    		}
+    		try {
+    			System.err.println("Clean up about to sleep...");
+				Thread.sleep(runFrequencyInMilliseconds);
+			} catch (InterruptedException e) {
+				System.err.println("Sleep failed");
+			}
+    	}
+   
+    }
+
+}
