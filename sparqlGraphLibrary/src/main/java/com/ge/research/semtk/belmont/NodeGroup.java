ce7a157a262fc55fdbaa6d0186c65dc85415d2f8
McHugh <200018594@logon.ds.ge.com>
2017-11-06 13:14:17 -0500

    commit message

--- sparqlGraphLibrary/src/main/java/com/ge/research/semtk/belmont/NodeGroup.java
+++ sparqlGraphLibrary/src/main/java/com/ge/research/semtk/belmont/NodeGroup.java
@@ -156,7 +156,7 @@ public class NodeGroup {
 		    			    	
 		        String key = keysItr.next();
 		        Object value = nodeJson.get(key);		        
-		        if(key.equals("@id") || key.equals("@type")){
+		        if(key.equals("@id") || key.equals("@type") || key.equals("@Original-SparqlId")){
 		        	continue;  // already got node id and type above, so skip
 		        }		        
 		        		        
