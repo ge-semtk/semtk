b9bc19b772d5652fb1a51d67e1aede5d00158cb2
Paul Cuddihy <cuddihy@research.ge.com>
2018-06-22 13:06:07 -0400

    full implementation of QueryFlags

--- sparqlGraphLibrary/src/main/java/com/ge/research/semtk/sparqlX/dispatch/QueryFlags.java
+++ sparqlGraphLibrary/src/main/java/com/ge/research/semtk/sparqlX/dispatch/QueryFlags.java
@@ -14,14 +14,22 @@ public class QueryFlags {
 	}
 	
 	public QueryFlags(String jsonStr) throws Exception {
-		if (jsonStr != null) {
-			JSONArray jsonArr = Utility.getJsonArrayFromString(jsonStr);
-			this.addFlags(jsonArr);
+		try {
+			if (jsonStr != null) {
+				JSONArray jsonArr = Utility.getJsonArrayFromString(jsonStr);
+				this.addFlags(jsonArr);
+			}
+		} catch (Exception e) {
+			throw new Exception("Invalid QueryFlags json array string: " + jsonStr, e);
 		}
 	}
 	
 	public QueryFlags(JSONArray jsonArr) throws Exception {
-		this.addFlags(jsonArr);
+		try {
+			this.addFlags(jsonArr);
+		} catch (Exception e) {
+			throw new Exception("Invalid QueryFlags json array: " + jsonArr.toJSONString(), e);
+		}
 	}
 	
 	private void addFlags(JSONArray jsonArr) {
@@ -37,7 +45,14 @@ public class QueryFlags {
 		return this.flags.isEmpty();
 	}
 	
-	public boolean contains(String flag) {
+	public void set (Enum flagVal) {
+		this.flags.add(flagVal.name());
+	}
+	public boolean isSet(Enum flagVal) {
+		return this.isSet(flagVal.name());
+	}
+	
+	public boolean isSet(String flag) {
 		return this.flags.contains(flag);
 	}
 	
