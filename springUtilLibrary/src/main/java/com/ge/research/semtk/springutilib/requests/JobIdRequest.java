ea17a7193095a647a4f9824598f6d38d6600b251
Paul Cuddihy <cuddihy@research.ge.com>
2018-08-31 10:22:33 -0400

    validate jobId

--- springUtilLibrary/src/main/java/com/ge/research/semtk/springutilib/requests/JobIdRequest.java
+++ springUtilLibrary/src/main/java/com/ge/research/semtk/springutilib/requests/JobIdRequest.java
@@ -19,10 +19,13 @@
 package com.ge.research.semtk.springutilib.requests;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class JobIdRequest {
-	@NotNull(message="id can not be null") @Size(min=4, message="id must be at least 4 chars")
+	@NotNull(message="jobId can not be null") 
+	@Size(min=4, message="jobId must be at least 4 chars")
+	@Pattern(regexp="[\\w\\d-]+", message="jobId must only contain letters, numbers, and [-]")
 	public String jobId;
 
 	public String getJobId() {
