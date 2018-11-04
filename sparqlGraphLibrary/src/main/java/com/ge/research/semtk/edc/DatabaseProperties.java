3112eca5f225f09c3cc25361ed69ff8e933e9e81
Jenny Williams <weisenje@research.ge.com>
2018-11-04 07:18:26 -0500

    Adding inheritable Properties class for database connection info

---
+++ sparqlGraphLibrary/src/main/java/com/ge/research/semtk/edc/DatabaseProperties.java
@@ -0,0 +1,59 @@
+/**
+ ** Copyright 2018 General Electric Company
+ **
+ **
+ ** Licensed under the Apache License, Version 2.0 (the "License");
+ ** you may not use this file except in compliance with the License.
+ ** You may obtain a copy of the License at
+ ** 
+ **     http://www.apache.org/licenses/LICENSE-2.0
+ ** 
+ ** Unless required by applicable law or agreed to in writing, software
+ ** distributed under the License is distributed on an "AS IS" BASIS,
+ ** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ** See the License for the specific language governing permissions and
+ ** limitations under the License.
+ */
+
+package com.ge.research.semtk.edc;
+
+// meant to be inherited into a spring boot properties object needing database connection info
+
+public abstract class DatabaseProperties {
+	private String server;
+	private int    port;
+	private String database;
+	private String user;
+	private String password;
+	
+	public String getServer() {
+		return server;
+	}
+	public void setServer(String server) {
+		this.server = server;
+	}
+	public int getPort() {
+		return port;
+	}
+	public void setPort(int port) {
+		this.port = port;
+	}
+	public String getDatabase() {
+		return database;
+	}
+	public void setDatabase(String database) {
+		this.database = database;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
