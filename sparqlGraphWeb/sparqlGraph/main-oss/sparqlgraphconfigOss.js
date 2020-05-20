856d92da7bee6012c8c3a94ea9924f7096ce2184
Paul Cuddihy <cuddihy@research.ge.com>
2020-05-20 13:43:53 -0400

    fix some drift bugs

--- sparqlGraphWeb/sparqlGraph/main-oss/sparqlgraphconfigOss.js
+++ sparqlGraphWeb/sparqlGraph/main-oss/sparqlgraphconfigOss.js
@@ -26,15 +26,15 @@ var g = {
 				     GE Research, Niskayuna<br>\
 				    ",
 
-		legalNoticeHtml : ' Copyright © 2014-2018  General Electric Company.  <br>\
-						    <b>Licensed under the Apache License, Version 2.0 (the "License") </b>  <br>\
+		legalNoticeHtml : " Copyright © 2014-2018  General Electric Company.  <br>\
+						    <b>Licensed under the Apache License, Version 2.0 (the License) </b>  <br>\
 						    you may not use this file except in compliance with the License.  <br>\
 						    You may obtain a copy of the License at  <br>\
 						    <br>\
-			 				<a href="http://www.apache.org/licenses/LICENSE-2.0" target="_blank">http://www.apache.org/licenses/LICENSE-2.0</a><br>\
+			 				<a href='http://www.apache.org/licenses/LICENSE-2.0' target='_blank'>http://www.apache.org/licenses/LICENSE-2.0</a><br>\
 						    <br>\
 						    Unless required by applicable law or agreed to in writing, software  \
-						    distributed under the License is distributed on an "AS IS" BASIS,  \
+						    distributed under the License is distributed on an 'AS IS' BASIS,  \
 						    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. \
 						    See the License for the specific language governing permissions and  \
 						    limitations under the License.  <br>\
@@ -43,14 +43,14 @@ var g = {
 						    <b>Additional Patents Pending.</b><br>\
 						    <br>\
 						    <u>Includes other open source code:</u><br>\
-						    - Vis.js (c) Almende B.V. licensed under Apache 2.0 and MIT
+						    - Vis.js (c) Almende B.V. licensed under Apache 2.0 and MIT\
 						    - (c) 2006 Aslak Hellesoy <aslak.hellesoy@gmail.com><br>\
 						    - (c) 2006 Dave Hoover <dave.hoover@gmail.com><br>\
 						    <br>\
 						    <i>Curry - Function currying.</i><br>\
 		    				Licensed under BSD (http://www.opensource.org/licenses/bsd-license.php)<br>\
 						    - Copyright (c) 2008 Ariel Flesler - aflesler(at)gmail(dot)com | http://flesler.blogspot.com<br>\
-						    ',
+						    ",
 
 		url : {
 			base : "https://github.com/ge-semtk/semtk/wiki",
