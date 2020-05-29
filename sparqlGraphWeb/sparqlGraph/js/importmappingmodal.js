bbb287a04058e05e3788d8e10fa746c5f6468cc4
Paul Cuddihy <cuddihy@research.ge.com>
2020-05-29 12:07:08 -0400

    fix bug in drop down when "Error if missing"

--- sparqlGraphWeb/sparqlGraph/js/importmappingmodal.js
+++ sparqlGraphWeb/sparqlGraph/js/importmappingmodal.js
@@ -6,9 +6,9 @@
  ** Licensed under the Apache License, Version 2.0 (the "License");
  ** you may not use this file except in compliance with the License.
  ** You may obtain a copy of the License at
- ** 
+ **
  **     http://www.apache.org/licenses/LICENSE-2.0
- ** 
+ **
  ** Unless required by applicable law or agreed to in writing, software
  ** distributed under the License is distributed on an "AS IS" BASIS,
  ** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,13 +24,13 @@ define([	// properly require.config'ed
     'sparqlgraph/js/importmapping',
     'sparqlgraph/js/selecttable',
     'jquery',
-    
+
     // shimmed
     'sparqlgraph/js/belmont',
     ],
 
 	function(ModalIidx, IIDXHelper, ImportMapping, SelectTable, $) {
-	
+
 		/**
 		 *  callback(OrderElement[])
 		 */
@@ -41,14 +41,14 @@ define([	// properly require.config'ed
             this.uriLookupSelect = null;
             this.uriLookupModeSelect = null;
             this.callback = callback;
-            
+
             this.div = null;
 		};
-		
-		
+
+
 		ImportMappingModal.prototype = {
-            
-            
+
+
             validateCallback : function() {
 				if (false) {
 					return "Something is wrong";
@@ -56,11 +56,11 @@ define([	// properly require.config'ed
 					return null;
 				}
 			},
-			
+
 			okCallback : function() {
                 var uriLookupSparqlIDs = IIDXHelper.getSelectValues(this.uriLookupSelect);
                 var uriLookupNodes = [];
-                
+
                 // change sparqlID's into belmont Nodes
                 for (var i=0; i < uriLookupSparqlIDs.length; i++) {
                     var node = this.importSpec.getNodegroup().getNodeBySparqlID(uriLookupSparqlIDs[i]);
@@ -69,9 +69,9 @@ define([	// properly require.config'ed
                     }
                     uriLookupNodes.push(node);
                 }
-                
+
                 this.mapping.setUriLookupNodes(uriLookupNodes);
-                
+
                 // URILookupMode (nodes only)
                 if (this.mapping.isNode()) {
                     var modeArr = IIDXHelper.getSelectValues(this.uriLookupModeSelect);
@@ -79,9 +79,9 @@ define([	// properly require.config'ed
                         throw new Error("Internal: multiple URI lookup modes for same node");
                     }
                     var mode = (modeArr.length == 0) ? null : modeArr[0];
-                
+
                     // keep the mode null if it is NO_CREATE and there is nothing actually looking it up
-                    if (this.mapping.getUriLookupMode() == null && 
+                    if (this.mapping.getUriLookupMode() == null &&
                         mode == ImportMapping.LOOKUP_MODE_NO_CREATE &&
                         this.importSpec.getLookupMappings(this.mapping).length == 0) {
                         mode = null
@@ -89,44 +89,44 @@ define([	// properly require.config'ed
 
                     this.mapping.setUriLookupMode(mode);
                 }
-                
+
                 this.callback();
 			},
-			
+
 			cancelCallback : function() {
-                
+
             },
-			
-            
+
+
             /**
               *
               */
             launch : function () {
                 var myName = this.mapping.getName();
-                
+
                 // get all possible lookup sparqlIds, excluding myName
                 var uriSparqlIDs = this.importSpec.getUriSparqlIDs();
                 var myIndex = uriSparqlIDs.indexOf(myName);
                 if (myIndex > -1) {
                     uriSparqlIDs.splice(myIndex, 1);
                 }
-                
+
                 // get current lookup sparql ids
                 var uriLookup = this.mapping.getUriLookupSparqlIDs();
-                
+
                 this.div = document.createElement("div");
-                
+
                 // URI Lookup title
                 IIDXHelper.appendTextLine(this.div, "Use " + myName + " to look up:");
-                
+
                 // URI Lookup select
                 this.uriLookupSelect = IIDXHelper.createSelect(null, uriSparqlIDs, uriLookup, true);
                 this.div.appendChild(this.uriLookupSelect);
                 this.div.appendChild(document.createElement("hr"));
-            
+
                 // URI Lookup Mode (nodes only)
                 if (this.mapping.isNode()) {
-                    
+
                     // Lookup Mode title
                     var lookingMeUp = this.importSpec.getLookupMappings(this.mapping);
                     var lookingMeUpStr = "";
@@ -141,14 +141,19 @@ define([	// properly require.config'ed
                     } else {
                         lookingMeUpStr = myName + " is not being looked up"
                     }
-                    
+
                     IIDXHelper.appendTextLine(this.div, lookingMeUpStr);
                     this.div.appendChild(document.createTextNode("Lookup mode: "));
-                    
+
                     // Lookup Mode select
-                    var modeStr = (this.mapping.getUriLookupMode() == ImportMapping.LOOKUP_MODE_CREATE) ? "Create if missing" : "Error if missing";
-                    this.uriLookupModeSelect = IIDXHelper.createSelect(null, 
-                                                                       [["Error if missing",  ImportMapping.LOOKUP_MODE_NO_CREATE], 
+                    var modeStr = "Error if missing";
+                    if (this.mapping.getUriLookupMode() == ImportMapping.LOOKUP_MODE_CREATE) {
+                        modeStr = "Create if missing";
+                    } else if (this.mapping.getUriLookupMode() == ImportMapping.LOOKUP_MODE_ERR_IF_EXISTS) {
+                        modeStr = "Error if exists";
+                    }
+                    this.uriLookupModeSelect = IIDXHelper.createSelect(null,
+                                                                       [["Error if missing",  ImportMapping.LOOKUP_MODE_NO_CREATE],
                                                                         ["Create if missing", ImportMapping.LOOKUP_MODE_CREATE   ],
                                                                         ["Error if exists", ImportMapping.LOOKUP_MODE_ERR_IF_EXISTS]],
                                                                        modeStr,
@@ -158,22 +163,22 @@ define([	// properly require.config'ed
                     IIDXHelper.removeMargins(this.uriLookupModeSelect);
                     this.div.appendChild(this.uriLookupModeSelect);
                 }
-                
+
                 // launch the modal
                 var m = new ModalIidx();
-                m.showOKCancel(	
+                m.showOKCancel(
                                 myName + " lookup settings",
                                 this.div,
                                 this.validateCallback.bind(this),
                                 this.okCallback.bind(this),
                                 this.cancelCallback.bind(this),
-                                "OK"); 
-                    
+                                "OK");
+
             },
-            
-            
+
+
 		};
-	
+
 		return ImportMappingModal;
 	}
 );
