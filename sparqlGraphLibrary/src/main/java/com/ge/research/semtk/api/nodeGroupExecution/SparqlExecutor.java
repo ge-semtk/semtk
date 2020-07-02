1e001ffe334f47aca06b163cada1566bfff34ec9
Paul Cuddihy <cuddihy@research.ge.com>
2020-07-02 09:43:33 -0400

    fixed timing bug.  Had been returning before job was created.

--- sparqlGraphLibrary/src/main/java/com/ge/research/semtk/api/nodeGroupExecution/SparqlExecutor.java
+++ sparqlGraphLibrary/src/main/java/com/ge/research/semtk/api/nodeGroupExecution/SparqlExecutor.java
@@ -4,10 +4,8 @@ import com.ge.research.semtk.auth.HeaderTable;
 import com.ge.research.semtk.auth.ThreadAuthenticator;
 import com.ge.research.semtk.edc.JobTracker;
 import com.ge.research.semtk.edc.client.ResultsClient;
-import com.ge.research.semtk.resultSet.ResultType;
 import com.ge.research.semtk.resultSet.SimpleResultSet;
 import com.ge.research.semtk.resultSet.Table;
-import com.ge.research.semtk.resultSet.TableResultSet;
 import com.ge.research.semtk.sparqlX.SparqlEndpointInterface;
 import com.ge.research.semtk.sparqlX.SparqlResultTypes;
 import com.ge.research.semtk.utility.LocalLogger;
@@ -19,48 +17,43 @@ public class SparqlExecutor extends Thread {
 	private ResultsClient resClient = null;
 	private String jobId = null;
 	private HeaderTable headerTable = null;
+	private JobTracker tracker = null;
 			
-	public SparqlExecutor(String sparql, SparqlEndpointInterface sei, SparqlEndpointInterface servicesSei, ResultsClient resClient) {
+	public SparqlExecutor(String sparql, SparqlEndpointInterface sei, SparqlEndpointInterface servicesSei, ResultsClient resClient) throws Exception {
 		this.sparql = sparql;
 		this.sei = sei;
 		this.servicesSei = servicesSei;
 		this.resClient = resClient;
 		this.jobId = JobTracker.generateJobId();
 		this.headerTable = ThreadAuthenticator.getThreadHeaderTable();
+		
+		// We're dead in the water if we can't track a job.  print stacktrace and return
+		
+		this.tracker = new JobTracker(this.servicesSei);
+		this.tracker.createJob(this.jobId);
+				
 	}
 	
 	public void run() {
 		ThreadAuthenticator.authenticateThisThread(this.headerTable);
 		
-		JobTracker tracker = null;
-		
-		// We're dead in the water if we can't track a job.  print stacktrace and return
-		try {
-			tracker = new JobTracker(servicesSei);
-			tracker.createJob(jobId);
-		} catch (Exception e) {
-			LocalLogger.printStackTrace(e);
-			return;
-		}
-		
-		// try the actual job
 		try {
 			
-			tracker.setJobPercentComplete(jobId, 5);
+			this.tracker.setJobPercentComplete(this.jobId, 5);
 			SimpleResultSet res = (SimpleResultSet) sei.executeQueryAndBuildResultSet(this.sparql, SparqlResultTypes.CONFIRM);
 			
-			tracker.setJobPercentComplete(jobId, 80);
+			this.tracker.setJobPercentComplete(this.jobId, 80);
 			
 			// put a table in the results for legacy SPARQLgraph
 			Table t = new Table(new String [] {"message"}, new String [] {"string"});
 			t.addRow(new String [] {res.getResult("@message")});
-			resClient.execStoreTableResults(jobId, t);
+			this.resClient.execStoreTableResults(this.jobId, t);
 			
-			tracker.setJobSuccess(jobId);
+			this.tracker.setJobSuccess(this.jobId);
 			
 		} catch (Exception e) {
 			try {
-				tracker.setJobFailure(jobId, e.toString());
+				this.tracker.setJobFailure(this.jobId, e.toString());
 			} catch (Exception ee) {
 				LocalLogger.printStackTrace(e);
 			}
