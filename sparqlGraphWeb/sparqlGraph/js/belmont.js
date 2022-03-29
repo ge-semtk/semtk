6b51b1667e384265b06c1c1ac5a0658261c9865c
Paul Cuddihy <cuddihy@research.ge.com>
2022-03-29 11:42:54 -0400

    ground work for ALLOW_CIRCULAR_NODEGROUPS.   turned OFF in master branch.

--- sparqlGraphWeb/sparqlGraph/js/belmont.js
+++ sparqlGraphWeb/sparqlGraph/js/belmont.js
@@ -1477,39 +1477,16 @@ SemanticNode.prototype = {
 		return ret;
 	},
 
-	getConnectedNodesForSparql : function() {
-		// return a list of connections as edges.
-		var k = this.nodeList.length;
-		var retval = [];
-
-		for (var i = 0; i < k; i++) {
-			nd = this.nodeList[i];
-			if (nd.getConnected()) {
-				var nodeConn = this.nodeList[i].getSNodes(); // get the nodes
-															// connecting to
-															// this locus
-				for (var d = 0; d < nodeConn.length; d++) {
-					var nxt = [ nd.getURIConnectBy(), nodeConn[d].getSparqlID() ];
-					retval.push(nxt);
-				}
-			}
-		}
-		return retval;
-	},
-
 	getConnectedNodes : function() {
 		// return a list of connections as SNodes.
-		var k = this.nodeList.length;
 		var retval = [];
 
-		for (var i = 0; i < k; i++) {
-			nd = this.nodeList[i];
-			if (nd.getConnected()) {
-				var nodeConn = this.nodeList[i].getSNodes(); // get the nodes
-															// connecting to
-															// this locus
-				for (var d = 0; d < nodeConn.length; d++) {
-					retval.push(nodeConn[d]);
+		for (var nItem of this.nodeList) {
+			if (nItem.getConnected()) {
+				for (var snode of nItem.getSNodes()) {
+					if (retval.indexOf(snode) == -1) {
+						retval.push(snode);
+					}
 				}
 			}
 		}
@@ -3047,18 +3024,22 @@ SemanticNodeGroup.prototype = {
 		return ret;
 	},
 
-	getSubNodes : function(topNode) {
+	getSubNodes : function(topNode, optStopList) {
+		var stopList = optStopList || [];
 		// recursive function returns topNode and all it's sub-nodes in a top
 		// down breadth-first search
 		var ret = [];
 
-		var conn = topNode.getConnectedNodes();
-		ret = ret.concat(conn);
-
-		for (var i = 0; i < conn.length; i++) {
-			var subs = this.getSubNodes(conn[i]);
-			ret = ret.concat(subs);
+		// add next level
+		var nextLevelList = topNode.getConnectedNodes();
+		for (var snode of nextLevelList) {
+			if (snode != topNode && stopList.indexOf(snode) == -1 && ret.indexOf(snode) == -1) {
+				ret.push(snode);
+				var subs = this.getSubNodes(snode, [topNode] + stopList + ret);
+				ret = ret.concat(subs);
+			}
 		}
+
 		return ret;
 	},
 
